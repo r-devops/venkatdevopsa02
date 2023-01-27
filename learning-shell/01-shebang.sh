165e01f7654a23387d6b36bd747c89c1454ff31f
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-01-27 07:12:33 -0500

    updated the script

--- learning-shell/01-shebang.sh
+++ learning-shell/01-shebang.sh
@@ -4,8 +4,13 @@
 # #! - is called she-bang 
 # optional -i fyou dont specefy it will execute as bash by default 
 
+echo "----------- Here is the hostname of the system ------------"
 hostname
+
+echo "----------- Here is the listing of files  ------------"
 ls -ltr
+
+echo "----------- Here is the date on the system ------------"
 date 
 
 # Shell script is adding the manual commands in a order 
