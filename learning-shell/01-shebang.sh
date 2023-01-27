a721b371607fc8ec9bdd223313735b5f310280e1
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-01-27 07:07:01 -0500

    01 script commited

---
+++ learning-shell/01-shebang.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# comment 
+# #! - is called she-bang 
+# optional -i fyou dont specefy it will execute as bash by default 
+
+hostname
+
+# Shell script is adding the manual commands in a order 
