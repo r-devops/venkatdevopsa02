80b8d63e4c1015cb41392c9970228096de092d6a
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-01-30 06:55:07 -0500

    updated 3

--- learning-shell/05-system-variables.sh
+++ learning-shell/05-system-variables.sh
@@ -2,3 +2,8 @@ echo $0 # Script name
 echo $1 # Argumemnt 1
 echo $2 # Argumemnt 2
 echo $4 # Argumemnt 4
+
+echo $* # All Arguments 
+echo $@ # All Arguments 
+
+echo $# # How many arguments have been passed 
