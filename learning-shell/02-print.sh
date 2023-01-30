4ed8a1b01fc869eb72f5bd0a1a8de984afdfec5a
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-01-30 06:28:06 -0500

    update 04

--- learning-shell/02-print.sh
+++ learning-shell/02-print.sh
@@ -1,3 +1,17 @@
 echo "Hello World"
 
-echo -e "\e[31m Hello World \e[0m"
+echo -e "\e[32m Hello World \e[0m"
+
+### Color print syntax 
+# -e to tell the script the you are using color codes 
+# \e[31m -> enable the color code 31
+# \e[0m -> This is to disable the color code 
+
+
+## color codes 
+# RED     31
+# Green   32
+# Yellow  33
+# Blue    34
+# Magneta 35
+# Cyan    36 
