4c801160ba6c07336b9af1adbee1272fb991d82d
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-01-31 11:37:55 +0000

    Jan 31, 2023, 6:37 AM

---
+++ learning-shell/08-case.sh
@@ -0,0 +1,18 @@
+# syntax
+# case $var in 
+# for  $pattern1 do below steps 
+# for $Pattern2 do below steps 
+
+
+system=$1
+
+case $system in 
+    linux)
+    echo "linux system"
+    ;;
+    unix) 
+    echo "Unix system"
+    ;;
+    *)
+    echo "Unknown system"
+esac    
