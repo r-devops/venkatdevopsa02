9e7ab272ca95caeb5fdcf0ca2b4a0d818616234e
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-02-02 11:54:42 +0000

    Feb 2, 2023, 6:54 AM

--- roboshop-shell-scripting/common.sh
+++ roboshop-shell-scripting/common.sh
@@ -1,7 +1,7 @@
 
 ID=$(id -u)
 
-if [ $ID -ne 0]; then 
+if [ $ID -ne 0 ]; then 
     echo "you are not running as root and this will fail"
     exit 1
 fi     
@@ -32,6 +32,9 @@ APP_PREREQ() {
     echo "Download ${COMPONENT} Application code"
     curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG_FILE}
     Statuscheck $?
+
+    echo "stoping the service before cleanup"
+    systemctl stop ${COMPONENT}.service
 
     echo " remove old content if exists - make the script compatable to re-run"
     cd /home/roboshop && rm -rf ${COMPONENT}  &>>${LOG_FILE}
