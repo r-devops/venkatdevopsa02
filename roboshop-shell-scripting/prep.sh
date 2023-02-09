c01edfbfdb178c6ee6e4dd61bec5df01f7f0a7ae
Venkat Kapisetti <venkat.kapisetti@gmail.com>
2023-02-09 11:30:08 +0000

    Feb 9, 2023, 6:30 AM

--- roboshop-shell-scripting/prep.sh
+++ roboshop-shell-scripting/prep.sh
@@ -1,6 +1,6 @@
 COMPONENT=prep 
 LOG_FILE=/tmp/${COMPONENT}
-source ./common.sh 
+#source ./common.sh 
 echo "disable selinux"
 sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config &>>${LOG_FILE}
 Statuscheck $?
@@ -8,3 +8,9 @@ Statuscheck $?
 echo "install git on it"
 yum install -y git  &>>${LOG_FILE}
 Statuscheck $?
+
+git clone https://github.com/r-devops/venkatdevopsa02.git
+
+sleep 10
+
+reboot 
--- roboshop-shell-scripting/vm-create.sh
+++ roboshop-shell-scripting/vm-create.sh
@@ -1,10 +1,10 @@
 
 echo " Create the VM's via CLI "
 
-catalogue user cart 
+catalogue user cart payment shipping 
 user cart payment shipping frontend mongodb redis mysql rabbitmq dispatch
 
-for component in payment shipping frontend mongodb redis mysql rabbitmq dispatch ; do
+for component in frontend mongodb redis mysql rabbitmq dispatch ; do
 az vm create --resource-group azure-training-2023 --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name azure-training-2023-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-address "" --size Standard_B1s
 az vm auto-shutdown -g azure-training-2023 -n  $component --time 1230  
 done
